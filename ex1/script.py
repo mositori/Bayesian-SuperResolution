042e15f5a5b1412fa6897b19f5912dff04ab5b73
Keisuke Shimotori <destrage1958@gmail.com>
2018-12-18 00:21:43 +0900

    add mnist_mlp

---
+++ ex1/script.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.layers import Input, Dense
+from keras.models import Model
+import tensorflow as tf
+
+# 入力用の変数 ( Q行列 )
+
+
+
+#autoencoder = Model(input_img,encoded)
