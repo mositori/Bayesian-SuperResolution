21f37a8918c5b6c7bcd12c77797293b6f63c5bf6
Keisuke Shimotori <destrage1958@gmail.com>
2018-12-18 17:30:09 +0900

    update

--- ex1/mnist_mlp.py
+++ ex1/mnist_mlp.py
@@ -18,6 +18,7 @@ epochs = 20
 
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+print(x_train.shape)
 
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
--- ex1/script.py
+++ ex1/script.py
@@ -1,10 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from keras.layers import Input, Dense
-from keras.models import Model
-import tensorflow as tf
+import itertools as iter
+#from keras.layers import Input, Dense
+#from keras.models import Model
+#import tensorflow as tf
 
-# 入力用の変数 ( Q行列 )
+# Generating P matrix
+# @type numpy.array
+print("Start to generate P matrix")
+
+isFirst = True
+for p_11, p_12, p_13, p_21, p_22, p_23, p_31, p_32, p_33 in iter.product([0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1]):
+  if isFirst == True:
+    _old = np.array([[p_11,p_12,p_13],[p_21, p_22, p_23],[p_31, p_32, p_33]])
+    isFirst = False
+    continue
+  else:
+    _new = np.array([[p_11,p_12,p_13],[p_21, p_22, p_23],[p_31, p_32, p_33]])
+  
+  _old = np.dstack((_old, _new))
+
+print(_old)
+# P matrix into Q matrix
 
 
 
