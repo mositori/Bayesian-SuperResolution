966b7cffbea4646ef58d457e8f62249efcd460ae
Keisuke Shimotori <destrage1958@gmail.com>
2018-12-23 14:35:57 +0900

    P行列生成コード追加

--- ex1/script.py
+++ ex1/script.py
@@ -4,22 +4,23 @@ import itertools as iter
 #from keras.models import Model
 #import tensorflow as tf
 
+# @return P matrix
+def generatePMatrix():
+  _temp = np.zeros((2 ** 9,3,3))
+  _count = 0
+  for p_11, p_12, p_13, p_21, p_22, p_23, p_31, p_32, p_33 in iter.product([0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1]):
+    _temp[_count] = np.array([[p_11, p_12, p_13],[p_21, p_22, p_23],[p_31, p_32, p_33]]) 
+    _count += 1
+  
+  return _temp
+
 # Generating P matrix
 # @type numpy.array
 print("Start to generate P matrix")
+P_matrix = generatePMatrix()
+print("Generated P matrix shaped " + str(P_matrix.shape))
 
-isFirst = True
-for p_11, p_12, p_13, p_21, p_22, p_23, p_31, p_32, p_33 in iter.product([0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1]):
-  if isFirst == True:
-    _old = np.array([[p_11,p_12,p_13],[p_21, p_22, p_23],[p_31, p_32, p_33]])
-    isFirst = False
-    continue
-  else:
-    _new = np.array([[p_11,p_12,p_13],[p_21, p_22, p_23],[p_31, p_32, p_33]])
-  
-  _old = np.dstack((_old, _new))
 
-print(_old)
 # P matrix into Q matrix
 
 
